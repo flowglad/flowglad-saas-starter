57d830bc5bfaa8dfd70d25bf00490be29ea84434
Agree Ahmed <agree.ahmed@gmail.com>
2025-06-26 14:27:53 -0400

    flowglad-ify

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,11 @@ import type { Metadata, Viewport } from 'next';
 import { Manrope } from 'next/font/google';
 import { UserProvider } from '@/lib/auth';
 import { getUser } from '@/lib/db/queries';
+import { FlowgladProvider } from '@flowglad/nextjs';
 
 export const metadata: Metadata = {
   title: 'Next.js SaaS Starter',
-  description: 'Get started quickly with Next.js, Postgres, and Stripe.',
+  description: 'Get started quickly with Next.js, Postgres, and Flowglad.',
 };
 
 export const viewport: Viewport = {
@@ -15,12 +16,12 @@ export const viewport: Viewport = {
 
 const manrope = Manrope({ subsets: ['latin'] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  let userPromise = getUser();
+  let user = await getUser();
 
   return (
     <html
@@ -28,7 +29,11 @@ export default function RootLayout({
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
     >
       <body className="min-h-[100dvh] bg-gray-50">
-        <UserProvider userPromise={userPromise}>{children}</UserProvider>
+        <FlowgladProvider loadBilling={!!user}>
+          <UserProvider userPromise={Promise.resolve(user)}>
+            {children}
+          </UserProvider>
+        </FlowgladProvider>
       </body>
     </html>
   );
--- lib/payments/actions.ts
+++ lib/payments/actions.ts
@@ -1,15 +1,17 @@
-'use server';
+'use server'
 
-import { redirect } from 'next/navigation';
-import { createCheckoutSession, createCustomerPortalSession } from './stripe';
-import { withTeam } from '@/lib/auth/middleware';
+import { redirect } from 'next/navigation'
+import { withTeam } from '@/lib/auth/middleware'
+import { flowgladServer } from '../flowglad'
 
 export const checkoutAction = withTeam(async (formData, team) => {
-  const priceId = formData.get('priceId') as string;
-  await createCheckoutSession({ team: team, priceId });
-});
-
-export const customerPortalAction = withTeam(async (_, team) => {
-  const portalSession = await createCustomerPortalSession(team);
-  redirect(portalSession.url);
-});
+  const priceId = formData.get('priceId') as string
+  const { url } = await flowgladServer.createCheckoutSession({
+    successUrl: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard`,
+    cancelUrl: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard`,
+    type: 'product',
+    priceId,
+    quantity: 1,
+  })
+  redirect(url)
+})
